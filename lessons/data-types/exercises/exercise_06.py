7cafc547a74be8d8bceb594f68e0a409a6e42d84
MathewStone23 <mateotomate2011@gmail.com>
2023-10-25 19:18:16 +0200

    exercises_done

---
+++ lessons/data-types/exercises/exercise_06.py
@@ -0,0 +1,9 @@
+my_list =[1,2,3,4,5]
+my_set = {-2,-1,0}
+my_string = "abc"
+my_list_2 =  ["I'm", "learning", "Python"]
+my_dict={
+    "seconds": 60*60*24,
+    "minutes": 60*24,
+    "hours":24,
+}
